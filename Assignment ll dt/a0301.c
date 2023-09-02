8f0b8afb1d56d2ec3c906c84fee5b572c9b5a14c
Ravindra Malviya <32036542+malviyaravi@users.noreply.github.com>
2023-09-02 17:02:31 +0530

    Add files via upload

---
+++ Assignment ll dt/a0301.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	int n;
+	int a[n];
+
+	scanf("%d", &n);
+
+	for(int i = 0; i < n; i++){
+	//	scanf("%d", &a[i]);
+	}
+	printf("''''''''''''''''''");
+
+	int *xxxxx = (int *)malloc(sizeof(int));
+	printf("%ld", sizeof(xxxxx));
+	return 0;
+}
